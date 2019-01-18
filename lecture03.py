d17aa12bb5df1aef3053f89dd54dd1a630997908
Lars Magnusson <lavima@gmail.com>
2019-01-18 08:38:48 +0100

    added comments and a minor update

--- lecture03.py
+++ lecture03.py
@@ -4,20 +4,29 @@ import numpy
 import sys
 from skimage import io,util,color
 
+# Import image using the first command line argument. The image is converted to 
+# grayscale before type conversion to float
 image = util.img_as_float(color.rgb2gray(io.imread(sys.argv[1])))
+# The constant multiplier is read from the second command line argument
 c = numpy.float(sys.argv[2])
+# The gamma value is read from the third command line argument
 gamma = numpy.float(sys.argv[3])
 
+# Iterate over each pixel in the image and store the transformed values
 #height, width = image.shape
 #out = numpy.zeros(image.shape, dtype=numpy.float)
 #for i in range(0,height):
 #  for j in range(0,width):
+     # This is the actual gamma transform of one pixel
 #    out[i,j] = c*image[i,j]**gamma
 
+# Vectorized version of the same as the above (preferred for runtime efficiency)
 out = c*image**gamma
 
+# Clip the values to the interval [0,1]
 out = numpy.clip(out, 0, 1)
 
+# Save the transformed image to the filename in the fourth command line argument
 outFile = sys.argv[4]
 io.imsave(outFile, out)
     
--- lecture03_02.py
+++ lecture03_02.py
@@ -1,18 +1,37 @@
-# imports needed for skimage and pyplot
+# Imports needed for skimage and pyplot
 import skimage
 import numpy
 import sys
 from skimage import io,util,color
 
+# Import image using the first command line argument. The image is converted to 
+# grayscale before type conversion to float
 image = util.img_as_float(color.rgb2gray(io.imread(sys.argv[1])))
+# The number of levels to use for the histogram
 num_levels = numpy.int(sys.argv[2])
 
+# Create an numpy.ndarray for our histogram. We could also use a list, but 
+# arrays have more efficient operations
 histogram = numpy.zeros((num_levels, 1), dtype=numpy.uint)
 
+# Loop through all the pixels in the image
 height, width = image.shape
 for i in range(0,height):
   for j in range(0,width):
-    histogram[int(round(image[i,j]*(num_levels-1)))] += 1 
+    # All values will be truncated towards zero (decimals removed), which will 
+    # make the last bin to collect only the extreme value 1
+    #bin_index = int(image[i,j]*(num_levels-1))
 
+    # This solution fixes this issue by rounding the values before truncing 
+    # them. This will make all the bins the same size, expect for the first and
+    # last, which are half the size of the other bins
+    # bin_index = int(round(image[i,j]*(num_levels-1)))
+
+    # This solution treats the extreme value of 1 as a special case. This
+    # solution has roughly equal sized bins.
+    bin_index = int(min(image[i,j]*num_levels, num_levels-1))
+
+    # Increase the count of the bin
+    histogram[bin_index] += 1 
 
 print(histogram)
