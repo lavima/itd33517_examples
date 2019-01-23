52180352069f2341428cd43b0406eb30ce4d00e9
Lars Magnusson <lars.v.magnusson@hiof.no>
2019-01-23 14:01:12 +0100

    lecture 4

--- lecture04_gaussian.py
+++ lecture04_gaussian.py
@@ -7,10 +7,18 @@ from skimage import io,util,color
 
 image = util.img_as_float64(color.rgb2gray(io.imread(sys.argv[1])))
 
-size = 3
+size = int(sys.argv[2])
+center = size/2
 
-filt = numpy.asarray([[1,2,1],[2,4,2],[1,2,1]], dtype=numpy.float)
-filt = filt / numpy.sum(filt) 
+def dist((i,j)):
+  (i-center,j-center) 
+
+def gaussian():
+  mask = numpy.ndarray((size,size), dtype=numpy.float) 
+  for i in range(0,size):
+    for j in range(0,size):
+      (s,t) = dist(i,j)
+      #mask[i,j] = equation
 
 print(filt)
 
