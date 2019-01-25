77f6f1029f863ef4ef787569e8ce0dd318b8c1fc
Lars Magnusson <lars.v.magnusson@hiof.no>
2019-01-25 07:02:40 +0100

    added comments

--- lecture04_average.py
+++ lecture04_average.py
@@ -6,19 +6,26 @@ import scipy
 from skimage import io,util,color
 from scipy import ndimage
 
+# Read image and convert it to grayscale and float
 image = util.img_as_float64(color.rgb2gray(io.imread(sys.argv[1])))
 
+# Read in the size of the filter and validate the value to be odd
 size = int(sys.argv[2])
 if (size%2==0):
   print("Wrong size")
   sys.exit()
 
+# Create the filter with the specified size
 filt = numpy.ones((size, size))
+# Normalize the values in the filter
 #filt = filt / numpy.sum(filt)
 filt = filt / size**2 
 
+# Show filter
 print(filt)
 
+# Perform the smoothing 
 out = scipy.ndimage.convolve(image, filt, mode="constant", cval=0)
 
+# Save the result
 io.imsave(sys.argv[3], numpy.clip(out, 0, 1))
--- lecture04_weighted_average.py
+++ lecture04_weighted_average.py
@@ -6,15 +6,18 @@ import scipy
 from skimage import io,util,color
 from scipy import ndimage
 
-image = util.img_as_float64(color.rgb2gray(io.imread(sys.argv[1])))
+# Read image and convert it to grayscale and float
+image = util.img_as_float(color.rgb2gray(io.imread(sys.argv[1])))
 
-size = 3
-
+# Create filter by specifying the values directly and normalizing 
 filt = numpy.asarray([[1,2,1],[2,4,2],[1,2,1]], dtype=numpy.float)
 filt = filt / numpy.sum(filt) 
 
+# Show filter
 print(filt)
 
+# Perform the smoothing 
 out = scipy.ndimage.convolve(image, filt, mode="constant", cval=0)
 
+# Save the result
 io.imsave(sys.argv[2], numpy.clip(out, 0, 1))
