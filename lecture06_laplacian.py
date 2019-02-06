4cc82c22b0272acf1542a4c37584cc91356d672d
Lars Magnusson <lars.v.magnusson@hiof.no>
2019-02-06 06:59:59 +0100

    Added comments

--- lecture06_laplacian.py
+++ lecture06_laplacian.py
@@ -9,14 +9,22 @@ from scipy import ndimage
 # Read in image and convert to appropriate format
 image = util.img_as_float(color.rgb2gray(io.imread(sys.argv[1])))
 
+# Read in the sharpening constant from command line
 c = float(sys.argv[2])
 
+# Create the Laplacian filter mask. This version of the filter generates positive 
+# values for brighter values. The negated alternative generates negative values for
+# brighter values. 
 mask = numpy.asarray([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
 
+# Filter the image with the filter
 laplacian = ndimage.convolve(image, mask, mode="nearest")
 
+# Perform the sharpening as specified. If the negated filter is used, one must 
+# subtract instead of add
 out = image + c*laplacian
 
+# Save the clipped result
 io.imsave(sys.argv[3], numpy.clip(out,0,1))
 
 
--- lecture06_sobel.py
+++ lecture06_sobel.py
@@ -9,12 +9,16 @@ from scipy import ndimage
 # Read in image and convert to appropriate format
 image = util.img_as_float(color.rgb2gray(io.imread(sys.argv[1])))
 
+# Create the two filter maskis
 sobel_x = numpy.asarray([[-1,0,1],[-2, 0, 2],[-1,0,1]])
 sobel_y = numpy.asarray([[-1,-2,-1],[0, 0, 0],[1,2,1]])
 
+# Concolve the image with both filters
 grad_x = ndimage.convolve(image, sobel_x, mode="nearest")
 grad_y = ndimage.convolve(image, sobel_y, mode="nearest")
 
+# Find the length of the vector made up of the two images
 out = numpy.sqrt(grad_x**2 + grad_y**2)
 
+# Save the clipped result
 io.imsave(sys.argv[2], numpy.clip(out,0,1))
