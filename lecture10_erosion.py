f74c2829877fc706fa5d193fe863d960c06bca12
Lars Magnusson <lars.v.magnusson@hiof.no>
2019-03-11 12:32:18 +0100

    Image resources

--- lecture10_erosion.py
+++ lecture10_erosion.py
@@ -5,17 +5,25 @@ import sys
 import numpy
 from skimage import util,io
 
+# Read in image and convert it to boolean
 image = util.img_as_bool(io.imread(sys.argv[1]))
+
+# Read in the structuring element (SE) and convert it to boolean
 se = util.img_as_bool(io.imread(sys.argv[2]))
 
+# Get the height and width of the SE
 se_height,se_width = se.shape
 
+# Find the center indices
 pad_v = se_height//2
 pad_h = se_width//2
 
+# Pad the input image with numpy.util.pad 
 padded = util.pad(image,(pad_v,pad_h),mode="constant")
 
+# Create output image
 out = numpy.zeros(image.shape,dtype=bool)
+
 for i in range(image.shape[0]):
   for j in range(image.shape[1]):
     subimage = padded[i:i+se_height,j:j+se_width]
